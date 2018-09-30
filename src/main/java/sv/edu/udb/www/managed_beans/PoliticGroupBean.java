6d30746bef4e7887ee3411a9cb40253514a50161
DiegoLem <ciegolem7@gmail.com>
2018-09-30 02:06:22 -0600

    Modulo de Admin General v 2.0
    - Subida de archivos
    - CRUD de partidos politicos
    - CRUD de departamentos

--- src/main/java/sv/edu/udb/www/managed_beans/PoliticGroupBean.java
+++ src/main/java/sv/edu/udb/www/managed_beans/PoliticGroupBean.java
@@ -5,6 +5,10 @@
  */
 package sv.edu.udb.www.managed_beans;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.List;
 import javax.ejb.EJB;
@@ -13,6 +17,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
+import javax.servlet.http.Part;
 import org.primefaces.event.FileUploadEvent;
 import sv.edu.udb.www.Entities.PoliticGroups;
 import sv.edu.udb.www.Model.PoliticGroupsModel;
@@ -32,6 +37,8 @@ public class PoliticGroupBean implements Serializable {
     
     private PoliticGroups politicGroup;
     
+    private Part image;
+    
     private String fileName;
     
     public String getFileName() {
@@ -40,6 +47,14 @@ public class PoliticGroupBean implements Serializable {
     
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public Part getImage() {
+        return image;
+    }
+
+    public void setImage(Part image) {
+        this.image = image;
     }
     
     public PoliticGroups getPoliticGroup() {
@@ -57,26 +72,46 @@ public class PoliticGroupBean implements Serializable {
     public void onloadRequest() {
         String code = Utilities.getParam("codigo");
         this.politicGroup = this.politicGroupModel.getPoliticGroup(Integer.parseInt(code));
+        this.fileName = this.politicGroup.getPhoto();
     }
-
+    public void doUpload(){
+        try{
+           InputStream in = this.image.getInputStream();
+           if(Validacion.esNombreImagen(this.image.getSubmittedFileName())){
+             File f = new File("C:\\Users\\Diego Lemus\\Desktop\\voting_App\\src\\main\\webapp\\images\\" + this.image.getSubmittedFileName());
+           this.politicGroup.setPhoto(this.image.getSubmittedFileName());
+           f.createNewFile();
+           FileOutputStream out = new FileOutputStream(f);
+           byte[] buffer = new byte[1024];
+           int length;
+           while((length = in.read(buffer)) > 0){
+               out.write(buffer, 0, length);
+           }
+           out.close();
+           in.close();  
+           }else{
+               Utilities.addMessageError("Error_Archivo", "Ingrese un archivo valido");
+           }
+           
+        }catch(IOException ex){
+            Utilities.addMessageError("Error_Archivo", "Ha ocurrido un error con el archivo");
+        }
+    }
     public void save() {
         if (Validacion.esNombreEmpresa(this.politicGroup.getName())) {
             if (this.politicGroup.getAcronym().length() >= 3 && this.politicGroup.getAcronym().length() <= 8) {
                 if (Validacion.esDireccion(this.politicGroup.getDescription())) {
-                    if (this.politicGroup.getPhoto().contains(".jpg") || this.politicGroup.getPhoto().contains(".png") || this.politicGroup.getPhoto().contains(".jpeg") || this.politicGroup.getPhoto().contains(".gif")) {
-                        if (this.politicGroupModel.existAcronym(politicGroup) && this.politicGroupModel.existName(politicGroup)) {
+                    this.doUpload();
+                        if (!this.politicGroupModel.existAcronym(politicGroup) && !this.politicGroupModel.existName(politicGroup)) {
                             if (this.politicGroupModel.insertPoliticGroup(this.politicGroup)) {
-                                Utilities.AddMessage("exito", "El Ciudadano fue ingresado!!");
+                                Utilities.AddMessage("exito", "El Partido politico fue ingresado!!");
                                 Utilities.redirect("/faces/generalAdministration/PoliticGroup.xhtml");
                             } else {
                                 Utilities.addMessageError("Error_Insert", "No se ha podido ingresar el partido politico");
                             }
                         } else {
-                            Utilities.addMessageError("Error_Existencia", "Revise el formato de la imagen");
+                            Utilities.addMessageError("Error_Existencia", "El nombre o el acronimo ya existen");
                         }
-                    } else {
-                        Utilities.addMessageError("Error_Imagen", "Revise el formato de la imagen");
-                    }
                 } else {
                     Utilities.addMessageError("Error_Direccion", "Algunos caracteres no estan permitidos en su descripcion");
                 }
@@ -90,24 +125,34 @@ public class PoliticGroupBean implements Serializable {
     public List<PoliticGroups> listPoliticGroups(){
         return this.politicGroupModel.listPoliticGroups();
     }
+    public void eliminateImage(File fichero){
+        if(!fichero.exists()){
+            Utilities.addMessageError("Error_Archivo", "El archivo con nombre " + fichero.getName() + " no existe");
+        }else{
+            fichero.delete();
+        }
+    }
     public void update() {
         if (Validacion.esNombreEmpresa(this.politicGroup.getName())) {
             if (this.politicGroup.getAcronym().length() >= 3 && this.politicGroup.getAcronym().length() <= 8) {
                 if (Validacion.esDireccion(this.politicGroup.getDescription())) {
-                    if (this.politicGroup.getPhoto().contains(".jpg") || this.politicGroup.getPhoto().contains(".png") || this.politicGroup.getPhoto().contains(".jpeg") || this.politicGroup.getPhoto().contains(".gif")) {
-                        if (this.politicGroupModel.existAcronym(politicGroup) && this.politicGroupModel.existName(politicGroup)) {
-                            if (this.politicGroupModel.editPoliticGroup(this.politicGroup)) {
-                                Utilities.AddMessage("exito", "El Ciudadano fue ingresado!!");
+                            if(this.image == null){
+                                if (this.politicGroupModel.editPoliticGroup(this.politicGroup)) {
+                                Utilities.AddMessage("exito", "El partido politico fue modificado!!");
                                 Utilities.redirect("/faces/generalAdministration/PoliticGroup.xhtml");
-                            } else {
-                                Utilities.addMessageError("Error_Insert", "No se ha podido ingresar el partido politico");
+                                } else {
+                                    Utilities.addMessageError("Error_Insert", "No se ha podido ingresar el partido politico");
+                                }
+                            }else{
+                                this.eliminateImage(new File("C:\\Users\\Diego Lemus\\Desktop\\voting_App\\src\\main\\webapp\\images\\" + this.politicGroup.getPhoto()));
+                                this.doUpload();
+                                if (this.politicGroupModel.editPoliticGroup(this.politicGroup)) {
+                                Utilities.AddMessage("exito", "El partido politico fue modificado!!");
+                                Utilities.redirect("/faces/generalAdministration/PoliticGroup.xhtml");
+                                } else {
+                                    Utilities.addMessageError("Error_Insert", "No se ha podido ingresar el partido politico");
+                                }
                             }
-                        } else {
-                            Utilities.addMessageError("Error_Existencia", "Revise el formato de la imagen");
-                        }
-                    } else {
-                        Utilities.addMessageError("Error_Imagen", "Revise el formato de la imagen");
-                    }
                 } else {
                     Utilities.addMessageError("Error_Direccion", "Algunos caracteres no estan permitidos en su descripcion");
                 }
@@ -134,6 +179,7 @@ public class PoliticGroupBean implements Serializable {
 
     public void delete(PoliticGroups politicGroup) {
         this.politicGroup = politicGroup;
+        this.eliminateImage(new File("C:\\Users\\Diego Lemus\\Desktop\\voting_App\\src\\main\\webapp\\images\\" + this.politicGroup.getPhoto()));
         if (this.politicGroupModel.deletePoliticGroup(this.politicGroup)) {
             Utilities.AddMessage("exito", "El Ciudadano fue eliminado!!");
             Utilities.redirect("/faces/generalAdministration/PoliticGroup.xhtml");
